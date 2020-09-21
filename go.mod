2b49c7e206361784c7073344ce67265064f0b8a3
Kento Takano <kento2github@gmail.com>
2020-09-21 20:37:34 +0900

    login route 追加

--- go.mod
+++ go.mod
@@ -3,6 +3,7 @@ module github.com/Kento75/bookstore_oauth-api
 go 1.14
 
 require (
+	github.com/federicoleon/golang-restclient v0.0.0-20191104170228-162ed620df66
 	github.com/gin-gonic/gin v1.6.3
 	github.com/gocql/gocql v0.0.0-20200815110948-5378c8f664e9
 	github.com/stretchr/testify v1.6.1
---
+++ src/repository/rest/users_repository.go
@@ -0,0 +1,56 @@
+package rest
+
+import (
+	"encoding/json"
+	"time"
+
+	"github.com/Kento75/bookstore_oauth-api/src/domain/access_token/utils/errors"
+	"github.com/Kento75/bookstore_oauth-api/src/domain/users"
+	"github.com/federicoleon/golang-restclient/rest"
+)
+
+var (
+	usersRestClient = rest.RequestBuilder{
+		BaseURL: "https://localhost:8082",
+		Timeout: 100 * time.Millisecond,
+	}
+)
+
+type RestUsersRepository interface {
+	LoginUser(string, string) (*users.User, *errors.RestErr)
+}
+
+type usersRepository struct{}
+
+func NewRepository() RestUsersRepository {
+	return &usersRepository{}
+}
+
+func (r *usersRepository) LoginUser(email string, password string) (*users.User, *errors.RestErr) {
+	request := users.UserLoginRequest{
+		Email:    email,
+		Password: password,
+	}
+	// README -> https://github.com/federicoleon/golang-restclient
+	response := usersRestClient.Post("/users/login", request)
+
+	if response == nil || response.Response == nil {
+		return nil, errors.InternalServerError("invalid restclient response when trying to login user")
+	}
+
+	if response.StatusCode > 299 {
+		var restErr errors.RestErr
+		err := json.Unmarshal(response.Bytes(), &restErr)
+
+		if err != nil {
+			return nil, errors.InternalServerError("invalid error interface when trying to login user")
+		}
+		return nil, &restErr
+	}
+
+	var user users.User
+	if err := json.Unmarshal(response.Bytes(), &user); err != nil {
+		return nil, errors.InternalServerError("error when trying to unmarshal users login response")
+	}
+	return &user, nil
+}
